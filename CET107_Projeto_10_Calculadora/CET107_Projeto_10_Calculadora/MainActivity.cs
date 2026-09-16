aba14972d26f80d3e935ce5e02dc446cec0f6734
Franciele Torres <mariafrancielesm@gmail.com>
2026-09-16 11:25:56 +0100

    Refactor calculator logic

--- CET107_Projeto_10_Calculadora/CET107_Projeto_10_Calculadora/MainActivity.cs
+++ CET107_Projeto_10_Calculadora/CET107_Projeto_10_Calculadora/MainActivity.cs
@@ -36,57 +36,115 @@ namespace CET107_Projeto_10_Calculadora
 
             if("0123456789.".Contains(button.Text))
             {
-                operandos[0] += button.Text;
-
-                calculator_text_viewC.Text = operandos[0];
+                AdicionaDigitoOuPontoDecimal(button.Text);
             }
             else if ("+-x/".Contains(button.Text))
             {
-                operacao = button.Text;
-
-                operandos[1] = operandos[0];
-                operandos[0] = "";
+               AdicionaOperador(button.Text);
             }
             else if(button.Text == "=")
             {
-                double resultado = 0;
-                double op1 = double.Parse(operandos[1]);
-                double op2 = double.Parse(operandos[0]);
-
-                switch (operacao)
-                {
-                    case "+":
-                        resultado = op1 + op2;
-                        break;
-                    case "-":
-                        resultado = op1 - op2;
-                        break;
-                    case "x":
-                        resultado = op1 * op2; 
-                        break;
-                    case "/":
-                        if (op2 != 0)
-                            resultado = op1 / op2;
-                        else
-                            calculator_text_viewC.Text = "Erro: Divisão por zero";
-                        break;
-                } 
-                
-                calculator_text_viewC.Text = resultado.ToString();
-
-                operandos[0] = "";
-                operandos[1] = "";
+                CalcularResultado();
                 operacao = "";
+                AtualizaCaixaDoResultado();
             }
             else if(button.Text == "DEL")
             {
-                operandos[0] = "";
-                operandos[1] = "";
-                operacao = "";
-                calculator_text_viewC.Text = "0";
+                LimpaParametros();
+                AtualizaCaixaDoResultado();
 
             }
 
         }
+
+
+        //Método auxiliar para armazenar a operacao selecionada pelo utilizador
+        //e para preparar para a entrada do segundo operando
+        private void AdicionaOperador(string operador)
+        {
+            if (operandos[0] == "")
+            {
+                return;
+            }
+            else if(operandos[1] != "")
+            {
+                CalcularResultado();
+                operacao = operador;
+               
+            }
+            else
+            {
+                operacao = operador;
+            }
+
+            AtualizaCaixaDoResultado();
+        }
+
+
+
+        private void AdicionaDigitoOuPontoDecimal(string texto)
+        {
+            int numOperando = (operacao == null || operacao == "") ? 0 : 1;
+
+            if(texto == "." && operandos[numOperando].Contains("."))
+            {
+                return;
+            }
+
+            operandos[numOperando] += texto;
+
+            AtualizaCaixaDoResultado();
+        }
+
+
+        //Obtem e apresenta o resultado da operacao atual
+        private void CalcularResultado()
+        {
+            double? resultado = null;
+
+            double? primeiroOperando = double.TryParse(operandos[0], out double op1) ? op1 : (double?)null;
+            double? segundoOperando = double.TryParse(operandos[1], out double op2) ? op2 : (double?)null;
+
+            switch (operacao)
+            {
+                case "+":
+                    resultado = op1 + op2;
+                    break;
+                case "-":
+                    resultado = op1 - op2;
+                    break;
+                case "x":
+                    resultado = op1 * op2;
+                    break;
+                case "/":
+                    if (op2 != 0)
+                        resultado = op1 / op2;
+                    else
+                        calculator_text_viewC.Text = "Erro: Divisão por zero";
+                    break;
+            }
+
+            if (resultado.HasValue)
+            {
+                operandos[0] = resultado.Value.ToString();
+                operandos[1] = "";
+
+            }
+
+        }
+
+        private void LimpaParametros()
+        {
+            operandos[0] = "";
+            operandos[1] = "";
+            operacao = "";
+
+        }
+
+        private void AtualizaCaixaDoResultado()
+        {
+            calculator_text_viewC.Text = operandos[0] + " " + operacao + " " + operandos[1];
+        }
+
     }
 }
